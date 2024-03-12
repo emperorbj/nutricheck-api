656c342b032886ffe98a9b1a0f1cfcd2ecfeba0a
BOLAJI OPATOLA <138551605+emperorbj@users.noreply.github.com>
2024-03-12 04:54:32 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+// importing user model
+const userModel = require('./models/userModel')
+
+const app = express()
+
+// for chunk by chunk functionality
+app.use(express.json())
+
+// database connection 
+mongoose.connect('mongodb://localhost:27017/Nutricheck')
+.then(()=>{
+    console.log('db connection okay')
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+app.post('/register',(req,res)=>{
+    let user = req.body;
+
+        bcrypt.genSalt(10,(err,salt)=>{
+            if(!err)
+            {
+                bcrypt.hash(user.password,salt,async (err,hpass)=>{
+                    if(!err)
+                    {
+                        user.password = hpass
+                        
+                        try
+                        {
+                            let doc = await userModel.create(user)
+                            res.status(201).send({message:'user registration successful'})
+                        }
+
+                        catch
+                        {
+                                console.log(err)
+                                res.status(500).send({message:'problem registering'})
+                        }
+
+
+                    }
+                })
+            }
+        })
+        
+})
+
+
+
+
+
+
+
+app.listen(8000,()=>{
+    console.log('server running')
+})
